cfc0a5b383b4b5f2e71f917f85053bcfebb390b3
Dusan Snokhaus <dusan.snokhaus@gmail.com>
2021-12-26 20:24:36 +0100

    Implement configuration decoupling

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from secrets import *
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,6 +25,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SECRET_KEY = config('SECRET_KEY', default='django-dev-1234')
 
 # Application definition
 
